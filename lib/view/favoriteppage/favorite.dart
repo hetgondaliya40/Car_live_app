5533c6126f686ef97bb83b8d3ce31eceaec9c189
hetgondaliya <132036737+hetgondaliya40@users.noreply.github.com>
2024-10-16 10:02:55 +0530

    1

--- lib/view/favoriteppage/favorite.dart
+++ lib/view/favoriteppage/favorite.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:new_appp_1_0/routes/routes.dart';
 import 'package:new_appp_1_0/view/screen/Detail/detail.dart';
-
-import '../screen/home_page/homepage2.dart';
 
 class FavoritePage extends StatefulWidget {
   const FavoritePage({super.key});
@@ -38,91 +37,105 @@ class _FavoritePageState extends State<FavoritePage> {
               itemCount: favList.length,
               padding: const EdgeInsets.all(16),
               itemBuilder: (context, index) => Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 150.h,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: Row(
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                  padding: const EdgeInsets.all(8.0),
+                  child: Slidable(
+                    endActionPane:
+                        ActionPane(motion: BehindMotion(), children: [
+                      SlidableAction(
+                        onPressed: (context) {
+                          favList.removeAt(index);
+                          setState(() {});
+                        },
+                        borderRadius: BorderRadius.circular(12),
+                        backgroundColor: Colors.red,
+                        icon: CupertinoIcons.delete,
+                      ),
+                    ]),
+                    child: Container(
+                      height: 150.h,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Row(
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                              left: 10,
-                            ),
-                            child: Container(
-                              height: 125.h,
-                              width: 150.w,
-                              decoration: BoxDecoration(
-                                color: Colors.grey.shade500,
-                                borderRadius: BorderRadius.circular(15.sp),
-                                image: DecorationImage(
-                                  image: FileImage(favList[index].image!),
-                                  fit: BoxFit.cover,
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 10,
                                 ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        width: 30.w,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10),
-                            child: Text(
-                              favList[index].maker.toString(),
-                              style: TextStyle(
-                                fontSize: 20.sp,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          Text(
-                            "Price : ${favList[index].askingPrice}".toString(),
-                            style: TextStyle(
-                              fontSize: 15.sp,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.pushNamed(context, Routes.cart);
-                            },
-                            child: Container(
-                              height: 50.h,
-                              width: 80.w,
-                              decoration: BoxDecoration(
-                                color: Color(0xff22262b),
-                                borderRadius: BorderRadius.circular(16),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  "Buy now",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 10.sp,
+                                child: Container(
+                                  height: 125.h,
+                                  width: 150.w,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.shade500,
+                                    borderRadius: BorderRadius.circular(15.sp),
+                                    image: DecorationImage(
+                                      image: FileImage(favList[index].image!),
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 30.w,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: Text(
+                                  favList[index].maker.toString(),
+                                  style: TextStyle(
+                                    fontSize: 20.sp,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                "Price : ${favList[index].askingPrice}"
+                                    .toString(),
+                                style: TextStyle(
+                                  fontSize: 15.sp,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(context, Routes.cart);
+                                },
+                                child: Container(
+                                  height: 50.h,
+                                  width: 80.w,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xff22262b),
+                                    borderRadius: BorderRadius.circular(16),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "Buy now",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 10.sp,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
-                  ),
-                ),
-              ),
+                    ),
+                  )),
             )
           : const Center(
               child: Text("Add Favorite"),
